2334d96ad52258a95fd2ea9a82be21b6e2b1fca5
Seoyun <manaallaire@outlook.com>
2025-07-26 02:44:26 +0800

    Updates

--- graph_8c.js
+++ graph_8c.js
@@ -2,10 +2,10 @@ var graph_8c =
 [
     [ "add_adjacency", "graph_8c.html#a345cb7b0c8682abb0fdc64afea79269d", null ],
     [ "clone_graph", "graph_8c.html#a49960767f684f34807da90b64b7282c7", null ],
-    [ "get_adjacency_count", "graph_8c.html#a5ade10246f4cf9546c36fc3e7854475a", null ],
+    [ "get_adjacency_count", "graph_8c.html#a830aa10be896c2390c417d56f972c368", null ],
     [ "get_edges", "graph_8c.html#a6997c695f57a7da0d95c7f61a12b2494", null ],
     [ "get_vertex_index", "graph_8c.html#ac4f32d439d20d81c7bd76389917daab5", null ],
-    [ "has_adjacency", "graph_8c.html#a7cc0b4f05ec1ed7cdbc266c5e0b070f2", null ],
+    [ "has_adjacency", "graph_8c.html#acc943972f5207ef2998b172b1701022c", null ],
     [ "has_edge", "graph_8c.html#ac8da2fa819b43ad1fa17ca79caddc8ed", null ],
     [ "has_vertex", "graph_8c.html#a7434068bb46d3954f6642846e98c8183", null ],
     [ "initialize_graph", "graph_8c.html#a26dc14efa9c8facd64a45e9c431e2611", null ],
